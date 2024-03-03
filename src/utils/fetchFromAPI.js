993d0a2b0058fb9d4a7d472429e27c7fe47c24e8
Arya Nath <nath.arya2000@gmail.com>
2024-03-03 22:46:29 +0530

    Updated

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'b539b21021msh7535eae47827803p195f21jsn81ff24568f36' ,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
